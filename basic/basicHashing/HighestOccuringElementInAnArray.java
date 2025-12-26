f1f265793cc4b4b0c33ff1714e9bb25638f12e97
Raghunandan-79 <raghunandansharma1905@gmail.com>
2025-12-26 15:10:01 +0530

    Highest Occuring Element in an Array

--- basic/basicHashing/HighestOccuringElementInAnArray.java
+++ basic/basicHashing/HighestOccuringElementInAnArray.java
@@ -8,23 +8,29 @@ public class HighestOccuringElementInAnArray {
     private static int mostFrequentElement(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
 
+        // adding elements to map
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
+        // initializing varibles to store max occuring element and max frequency
         int maxElement = Integer.MIN_VALUE, maxFreq = Integer.MIN_VALUE;
 
+        // iterating through the map
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            // variables to check for current element and frequency
             int element = entry.getKey(), freq = entry.getValue();
 
+            // checking if frequency is greater than max frequency
             if (freq > maxFreq) {
                 maxElement = element;
                 maxFreq = freq;
-            } else if (freq == maxFreq) {
+            } else if (freq == maxFreq) { // checking if the frequency is equal to max frequency
                 maxElement = Math.min(maxElement, element);
             }
         }
 
+        // return the most occuring element
         return maxElement;
     }
     
