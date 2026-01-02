2ff81dbaff9ab51155e2a89e23741ec5bf6f6ff2
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-02 19:33:58 +0530

    Find Missing Number better approach

--- arrays/logicBuilding/FindMissingNumber.java
+++ arrays/logicBuilding/FindMissingNumber.java
@@ -6,29 +6,23 @@ public class FindMissingNumber {
     private static int missingNumber(int[] nums) {
         int n = nums.length;
 
-        // iterating from 0 to n
+        // Array to store frequencies, initialized to 0
+        int[] freq = new int[n + 1];
+
+        // Storing the frequencies in the array
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Checking the frequencies for numbers 0 to N
         for (int i = 0; i <= n; i++) {
-            // taking flag as 0 to get missing number
-            int flag = 0;
-
-            // iterating the array
-            for (int j = 0; j < n; j++) {
-                // if element at j index is equal to i
-                if (nums[j] == i) {
-                    // updating flag as 1 and breaking the loop
-                    flag = 1;
-                    break;
-                }
-            }
-
-            // if flag is 0 returning i index
-            if (flag == 0) {
+            if (freq[i] == 0) {
                 return i;
             }
         }
 
-        // returning 0 at end as 0 is missing
-        return 0;
+        // This line will never execute, it is just to avoid warnings
+        return -1;
     }
 
     public static void main(String[] args) {
